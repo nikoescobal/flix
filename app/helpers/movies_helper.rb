0344640c3cfe2efa6aebdf496a86416df2a5799e
Niko Escobal <niko.escobal@gmail.com>
2021-06-01 19:36:24 +0800

    Add custom method to view helper to display 'flop' for movies with total gross below 225,000,000.

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,13 @@
 module MoviesHelper
+  def total_gross(movie)
+    if movie.flop?
+      'Flop!'
+    else
+      number_to_currency(movie.total_gross, precision: 0, delimiter: ', ')
+    end
+  end
+
+  def year_of(movie)
+    movie.released_on.strftime("%Y")
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  def flop?
+    total_gross.blank? || total_gross < 225_000_000
+  end
 end
