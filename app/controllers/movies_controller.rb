1a16b92e196f8010c87b5403b1fee6aa304fa0bd
Niko Escobal <niko.escobal@gmail.com>
2021-06-03 19:40:03 +0800

    Add movie_params private method and create method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,11 +13,24 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = 
-      params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
     @movie.update(movie_params)
-    redirect_to :movie
+    redirect_to @movie
   end
 
+  def new
+    @movie = Movie.new
+  end
+  
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  root "movies#index"
-  get "movies", to: "movies#index"
-  get "movies/:id", to: "movies#show", as: "movie"
-  get "movies/:id/edit", to: "movies#edit", as: "edit_movie"
-  patch "movies/:id", to: "movies#update"
+  root 'movies#index'
+  resources :movies
 end
