a50b0a628d3694adc267556a7a6555597a302803
Niko Escobal <niko.escobal@gmail.com>
2021-06-04 01:02:18 +0800

    Add partials for footer and header

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,11 +20,15 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
-  
+
   def create
     @movie = Movie.new(movie_params)
     @movie.save
     redirect_to @movie
+  end
+
+  def destroy
+    @movie.destroy
   end
 
   private
