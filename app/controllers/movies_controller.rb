2e14f9d644f4d3b5b76b6bdccd18ce6f1a36cde6
Niko Escobal <niko.escobal@gmail.com>
2021-06-03 18:23:33 +0800

    Add edit and update methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = 
+      params.require(:movie).
+        permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie.update(movie_params)
+    redirect_to :movie
+  end
+
 end
