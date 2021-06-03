aaea44f08ef15b18a2ffae1f15a98988fc1cbd60
Niko Escobal <niko.escobal@gmail.com>
2021-06-04 04:43:29 +0800

    Update delete method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -28,7 +28,9 @@ class MoviesController < ApplicationController
   end
 
   def destroy
+    @movie = Movie.find(params[:id])
     @movie.destroy
+    redirect_to movies_url
   end
 
   private
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ApplicationRecord
+
+  def self.released
+    where("relleased_on == true")
+    
+  end
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
