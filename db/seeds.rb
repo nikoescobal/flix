16a9ff94e98543076950062b8675a2dcfda941a8
Niko Escobal <niko.escobal@gmail.com>
2021-06-08 23:05:57 +0800

    Updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,228 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create!([
+  {
+    title: 'Apocalypto',
+    description:
+    %{
+      As the Mayan kingdom faces its decline, a young man is taken on a perilous journey to a world ruled by fear and oppression.
+    }.squish,
+    released_on: "2006-12-08",
+    rating: 'R',
+    total_gross: 120_673_227,
+    director: 'Mel Gibson',
+    duration: '139 min',
+    image_file_name: 'apocalypto.png'
+  },
+  {
+    title: 'The Shawshank Redemption',
+    description: 
+    %{
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.
+    }.squish,
+    released_on: "1994-10-14",
+    rating: 'R',
+    total_gross: 288_172_910, 
+    director: 'Frank Darabont',
+    duration: '142 min',
+    image_file_name: 'shawshank-redemption.png'
+  },
+  {
+    title: 'Inception',
+    description:
+    %{
+      A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.
+    }.squish,
+    released_on: "2011-06-13",
+    rating: 'PG-13',
+    total_gross: 836_836_967,
+    director: 'Christopher Nolan',
+    duration: '139 min',
+    image_file_name: 'inception.png'
+  },
+  {
+    title: 'Avatar',
+    description:
+    %{
+      A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.
+    }.squish,
+    released_on: "2009-12-18",
+    rating: 'PG-13',
+    total_gross: 2_847_246_203,
+    director: 'James Cameron',
+    duration: '162 min',
+    image_file_name: 'avatar.png'
+  },
+  {
+    title: 'Avengers: Endgame',
+    description:
+    %{
+      After the devastating events of Avengers: Infinity War, the universe
+      is in ruins. With the help of remaining allies, the Avengers assemble
+      once more in order to undo Thanos' actions and restore order to the universe.
+    }.squish,
+    released_on: "2019-04-26",
+    rating: 'PG-13',
+    total_gross: 1_223_641_414,
+    director: 'Anthony Russo',
+    duration: '181 min',
+    image_file_name: 'avengers-end-game.png'
+  },
+  {
+    title: 'Captain Marvel',
+    description:
+    %{
+      Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.
+    }.squish,
+    released_on: "2019-03-08",
+    rating: 'PG-13',
+    total_gross: 1_110_662_849,
+    director: 'Anna Boden',
+    duration: '124 min',
+    image_file_name: 'captain-marvel.png'
+  },
+  {
+    title: 'Black Panther',
+    description:
+    %{
+      T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.
+    }.squish,
+    released_on: "2018-02-16",
+    rating: 'PG-13',
+    total_gross: 1_346_913_161,
+    director: 'Ryan Coogler',
+    duration: '134 min',
+    image_file_name: 'black-panther.png'
+  },
+  {
+    title: 'Avengers: Infinity War',
+    description:
+    %{
+      The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.
+    }.squish,
+    released_on: "2018-04-27",
+    rating: 'PG-13',
+    total_gross: 2_048_359_754,
+    director: 'Anthony Russo',
+    duration: '149 min',
+    image_file_name: 'avengers-infinity-war.png'
+  },
+  {
+    title: 'Green Lantern',
+    description:
+    %{
+      Reckless test pilot Hal Jordan is granted an alien ring that bestows him with otherworldly powers that inducts him into an intergalactic police force, the Green Lantern Corps.
+    }.squish,
+    released_on: "2011-06-17",
+    rating: 'PG-13',
+    total_gross: 219_851_172,
+    director: 'Martin Campbell',
+    duration: '114 min',
+    image_file_name: 'green-lantern.png'
+  },
+  {
+    title: 'Fantastic Four',
+    description:
+    %{
+      Four young outsiders teleport to an alternate and dangerous universe which alters their physical form in shocking ways. The four must learn to harness their new abilities and work together to save Earth from a former friend turned enemy.
+    }.squish,
+    released_on: "2015-08-07",
+    rating: 'PG-13',
+    total_gross: 168_257_860,
+    director: 'Josh Trank',
+    duration: '100 min',
+    image_file_name: 'fantastic-four.png'
+  },
+  {
+    title: 'Iron Man',
+    description:
+    %{
+      When wealthy industrialist Tony Stark is forced to build an
+      armored suit after a life-threatening incident, he ultimately
+      decides to use its technology to fight against evil.
+    }.squish,
+    released_on: "2008-05-02",
+    rating: 'PG-13',
+    total_gross: 585_366_247,
+    director: 'Jon Favreau',
+    duration: '126 min',
+    image_file_name: 'ironman.png'
+  },
+  {
+    title: 'Superman',
+    description:
+    %{
+      An alien orphan is sent from his dying planet to Earth, where
+      he grows up to become his adoptive home's first and greatest
+      super-hero.
+    }.squish,
+    released_on: "1978-12-15",
+    rating: 'PG',
+    total_gross: 300_451_603,
+    director: 'Richard Donner',
+    duration: '143 min',
+    image_file_name: 'superman.png'
+  },
+  {
+    title: 'Spider-Man',
+    description:
+    %{
+      When bitten by a genetically modified spider, a nerdy, shy, and
+      awkward high school student gains spider-like abilities that he
+      eventually must use to fight evil as a superhero after tragedy
+      befalls his family.
+    }.squish,
+    released_on: "2002-05-03",
+    rating: 'PG-13',
+    total_gross: 825_025_036,
+    director: 'Sam Raimi',
+    duration: '121 min',
+    image_file_name: 'spiderman.png'
+  },
+  {
+    title: 'Batman',
+    description:
+    %{
+      The Dark Knight of Gotham City begins his war on crime with his
+      first major enemy being the clownishly homicidal Joker.
+    }.squish,
+    released_on: "1989-06-23",
+    rating: 'PG-13',
+    total_gross: 411_348_924,
+    director: 'Tim Burton',
+    duration: '126 min',
+    image_file_name: 'batman.png'
+  },
+  {
+    title: "Catwoman",
+    description:
+    %{
+      Patience Philips seems destined to spend her life apologizing for taking up space. Despite her artistic ability she has a more than respectable career as a graphic designer.
+    }.squish,
+    released_on: "2004-07-23",
+    rating: "PG-13",
+    total_gross: 82_102_379,
+    director: "Jean-Christophe 'Pitof' Comar",
+    duration: "101 min",
+    image_file_name: "catwoman.png"
+  },
+  {
+    title: "Wonder Woman",
+    description:
+    %{
+      When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.
+    }.squish,
+    released_on: "2017-06-02",
+    rating: "PG-13",
+    total_gross: 821_847_012,
+    director: "Patty Jenkins",
+    duration: "141 min",
+    image_file_name: "wonder-woman.png"
+  }
+])
