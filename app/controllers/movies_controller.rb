d5363f9e3e8b65a072fe008e3c715935b2011c6f
Niko Escobal <niko.escobal@gmail.com>
2021-06-09 23:04:02 +0800

    Add validations for movies and set up error display messages.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,8 +13,11 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to @movie
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit
+    end
   end
 
   def new
@@ -23,8 +26,11 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
+    end
   end
 
   def destroy
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,13 @@
 class Movie < ApplicationRecord
   validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image"
+  }
+  RATINGS = %w(G PG  PG-13 R NC-17)
+  validates :rating, inclusion: { in: RATINGS }
 
   def self.released
     where("released_on == true")
