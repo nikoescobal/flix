76156eb2b882e94e79a6491c74002f0c29651133
Niko Escobal <niko.escobal@gmail.com>
2021-06-10 17:06:26 +0800

    Add flash to partial

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
-      redirect_to @movie
+      redirect_to @movie, notice: 'Movie successfully updated!'
     else
       render :edit
     end
